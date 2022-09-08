e7b49b912a8aeace6246a68efe6ef49387276b2f
Aleksandr <alex_l_eon@mail.ru>
2022-09-09 00:08:22 +0300

    1
    1

---
+++ WpfApplication2/About.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ConstrLeo
+{
+    /// <summary>
+    /// Логика взаимодействия для About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            txtBl_about.Text = "4589475987";
+        }
+
+    }
+}
